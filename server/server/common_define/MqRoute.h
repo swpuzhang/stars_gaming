f0820ef6fb81c41a630b08363ac3888e808bc645
swpuzhang <840757167@qq.com>
2018-11-19 03:10:31 +0800

    111

---
+++ server/server/common_define/MqRoute.h
@@ -0,0 +1,7 @@
+#ifndef MQ_ROUTE_H
+#define MQ_ROUTE_H
+
+const char EXCHANGE_LOGIN_TOPIC[] = "login.topic";
+const char ROUTE_USER_LOGIN[] = "user_login";
+#endif // !MQ_ROUTE_H
+
---
+++ server/server/common_define/UserCommon.h
@@ -0,0 +1,25 @@
+#ifndef USER_COMMON
+#define  USER_COMMON
+#include "libtools/JsonParser.h"
+#include "FiledClass.h"
+FIELD_CLASS2(AccountInfo,
+	StringType, account,
+	IntType, user_id);
+
+FIELD_CLASS13(UserInfo,
+	StringType, account,
+	StringType, device_code,
+	StringType, device_name,
+	IntType, app_type,
+	IntType, user_id,
+	IntType, user_sex,
+	StringType, user_name,
+	StringType, header_url,
+	IntType, user_level,
+	IntType, user_cur_exp,
+	IntType, need_exp,
+	IntType, last_login_time,
+	IntType, last_offline_time);
+
+
+#endif
